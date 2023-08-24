8a6aa0e04d23b6e3043ebea0f02770f9741d4be2
tsemharb <gorbachevart@gmail.com>
2023-08-24 12:26:08 +0300

    time to modality

--- R/generateTreatmentModalitiesTimeToEventInfo.R
+++ R/generateTreatmentModalitiesTimeToEventInfo.R
@@ -7,11 +7,16 @@ generateTreatmentModalitiesTimeToEventInfo <- function(connection, cohortDatabas
                              getTargetStrataXref() %>% dplyr::filter(startsWith(as.character(cohortId), '101')) %>% dplyr::pull('cohortId'))
   }
   
-  treatmentCohortsIds <- read.csv(file.path('inst/settings/CohortsToCreateTarget.csv')) %>%
-    dplyr::filter(cohortId %in% c(107, 108, 110, 111, 112, 113)) %>% 
+  treatmentCohortsIds <- read.csv(file.path('inst/settings/CohortsToCreateModality.csv')) %>%
     dplyr::mutate(eventId = name) %>%
     dplyr::rename('outcomeCohortIds' = 'name', 'name' = 'atlasName') %>% 
     dplyr::select(outcomeCohortIds, name, eventId)
+  allModalitiesIds <- treatmentCohortsIds %>% dplyr::pull(outcomeCohortIds)
+  treatmentCohortsIds <- rbind(treatmentCohortsIds,
+                               data.frame(outcomeCohortIds = paste(allModalitiesIds, collapse = ', '),
+                                          name = 'Any modality',
+                                          eventId = max(allModalitiesIds) + 1)
+  )
   
   timeToTreatmentModality <- generateSurvival(connection = connection,
                                               cohortDatabaseSchema = cohortDatabaseSchema,
@@ -23,3 +28,5 @@ generateTreatmentModalitiesTimeToEventInfo <- function(connection, cohortDatabas
   return(timeToTreatmentModality)
   
 }
+
+
