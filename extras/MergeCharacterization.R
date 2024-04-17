e48be24a6ad07c579d3feeca08b7144dec3d0c10
tsemharb <gorbachevart@gmail.com>
2024-04-17 15:01:15 +0300

    merge remove demo code

--- extras/MergeCharacterization.R
+++ extras/MergeCharacterization.R
@@ -1,7 +1,7 @@
 library(Andromeda)
 library(dplyr)
 
-folderName <- 'C:\\Users\\Artem\\R_studies_results\\PIONEER_Treatment_v4.0'
+folderName <- 'C:\\Users\\Artem\\R_studies_results\\FUP'
 files <- list.files(folderName, pattern = "\\.zip$")
 
 if (length(files) == 0) {
@@ -32,40 +32,3 @@ for (file in files[-1]) {
 }
 
 saveAndromeda(studyResults, file.path(folderName, 'study_results.zip'))
-
-
-# library(Andromeda)
-# df1 <- andromeda(tab = data.frame(x = runif(4000000), y = runif(4000000), 
-#                                   xx = runif(4000000), yy = runif(4000000), 
-#                                   xxx = runif(4000000), yyy = runif(4000000)))
-# df2 <- andromeda(tab = data.frame(x = runif(4000000), y = runif(4000000),
-#                                   xx = runif(4000000), yy = runif(4000000),
-#                                   xxx = runif(4000000), yyy = runif(4000000)))
-# appendToTable(df1[['tab']], df2[['tab']])
-
-
-
-
-
-
-library(Andromeda)
-folderName <- 'C:\\Users\\Artem\\R_studies_results\\PIONEER_Treatment_v4.0'
-files <- list.files(folderName, pattern = "\\.zip$")
-studyResults <- loadAndromeda(file.path(folderName, files[1]))
-andrData <- loadAndromeda(file.path(folderName, files[2]))
-
-
-a <- andromeda(tte = studyResults$cohort_time_to_event)
-a$cv <- studyResults$covariate_value
-
-
-b <- andromeda(tte = andrData$cohort_time_to_event)
-b$cv <- andrData$covariate_value
-
-
-appendToTable(a[['cv']], b[['cv']])
-appendToTable(a[['tte']], b[['tte']])
-
-
-
-
